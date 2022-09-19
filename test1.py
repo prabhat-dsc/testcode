3106feed84f5f35d294b2c2b10625250e96e4461
prabhat-dsc <prabhat.dsc48@gmail.com>
2022-09-19 08:19:20 +0530

    This is first commit from pycharm

---
+++ test1.py
@@ -0,0 +1,12 @@
+import  pymongo
+client = pymongo.MongoClient("mongodb+srv://prabhat:mongodb123@cluster0.iean9di.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+d={
+    "name":'ravi',
+    "surname":'teja',
+    "job":'Big data'
+}
+db1=client['mongotest']
+coll=db1['test']
+coll.insert_one(d)
